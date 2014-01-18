b8d165147d9cfb4fef8ed67deafa5e0d8390461e
masums <masums@gmail.com>
2014-01-18 22:40:00 +0600

    Added phalcon.so compiled on openshift redhad linux

---
+++ web/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>How to Install phalcon php on openshift </title>
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/favicon.ico" type="image/x-icon">
+	</head>
+	<body>
+		echo "<h1> Welcom to phalcon php framework development at OpenShift<hr/>";
+		echo "Please check phalcon extension below";
+		phpinfo();
+	</body>
+</html>
+<?php
+
+?>
